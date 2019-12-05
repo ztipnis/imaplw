67d70ac36e74b87414b9eafad91909bdd9656b9c
Zachary Tipnis <tipniz@rpi.edu>
2019-12-05 01:08:21 -0500

    tls/ssl support & some cleanup of the codebase

--- IMAP.cpp
+++ IMAP.cpp
@@ -9,7 +9,8 @@ int main(int argc, char* argv[]){
 	std::cout << "Note: if # clients is not evently divisible by # threads, # clients will be truncated to be distibuted evenly amongst threads." << std::endl;
 	unsigned int port = 8080;
 	const char* address = "0.0.0.0";
-	IMAPProvider::IMAPProvider p = IMAPProvider::IMAPProvider();
+	IMAPProvider::Config c(true, false, "all", "server.key", "server.crt");
+	IMAPProvider::IMAPProvider p = IMAPProvider::IMAPProvider(c);
 	SocketPool sp(port, address, atoi(argv[1]), atoi(argv[2]), p);
 	while(1){
 		sp.listen(std::chrono::minutes(0));
--- IMAP/Providers.hpp
+++ IMAP/Providers.hpp
@@ -1,225 +1,110 @@
-#import <iostream>
+// #import <iostream>
 #import <string>
 #import <map>
 #import <sstream>
 #import <uuid/uuid.h>
 #import <cstdlib>
-#include <algorithm>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
+#import <algorithm>
+#import <tls.h>
 #import "../SocketPool/SocketPool.hpp"
-
-
-
-std::string gen_uuid(int len){
-	uuid_t id;
-	uuid_generate( (unsigned char *)&id );
-	srand(1);
-	char *uuid = new char[len + 1];
-	uuid[len] = '\0';
-	const char alphanum[36] = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-								'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-								'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-								'u', 'v', 'w', 'x', 'y', 'z'};
-	for(int i = 0; i < len; i++){
-		srand(reinterpret_cast<unsigned int &>(id[i % 16]) + rand());
-		uuid[i] = alphanum[rand() % 36];
-	}
-	std::string uuid_r(uuid);
-	return uuid_r;
-}
-
-
-class AuthenticationProvider {
-public:
-	virtual bool lookup(std::string username) = 0;
-	virtual bool authenticate(std::string username, std::string password) = 0;
-	template <class T> static AuthenticationProvider& getInst(){
-		static T m_Inst;
-		return m_Inst;
-	}
-private:
-	AuthenticationProvider(AuthenticationProvider const&) = delete;
-	AuthenticationProvider& operator=(AuthenticationProvider const&) = delete;
-	AuthenticationProvider() = delete;
-};
-
-//Dataprovider Subclass must provide init() to initialize m_Inst and implement all public functions.
-class DataProvider {
-public:
-	template <class T> static DataProvider& getInst(){
-		static T m_Inst;
-		return m_Inst;
-	}
-	virtual std::string get() = 0;
-private:
-	DataProvider(DataProvider const&) = delete;
-	DataProvider& operator=(DataProvider const&) = delete;
-	DataProvider() = delete;
-};
-
-inline void sendMsg(int fd, const std::string &data){
-	#ifndef SO_NOSIGPIPE
-		send(fd, &data[0], data.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
-	#else
-		send(fd, &data[0], data.length(), MSG_DONTWAIT);
-	#endif
-}
-
+#import "config.hpp"
+#import "Helpers.hpp"
+#import "IMAPClientState.hpp"
 
 namespace IMAPProvider{
-	typedef enum { UNENC, UNAUTH, AUTH, SELECTED } IMAPState_t;
-
-	//template <class A>
-	class IMAPClientState {
-	private:
-		std::string uuid;
-		bool encrypted;
-		bool authenticated;
-		std::string user;
-		bool selected;
-		std::string mbox;
-	public:
-		IMAPClientState(){
-			encrypted = false;
-			authenticated = false;
-			user = "";
-			selected = false;
-			mbox = "";
-			uuid = gen_uuid(15);
-		}
-		const IMAPState_t state() const{
-			if(!encrypted){
-				return UNENC;
-			}else if(authenticated){
-				if(selected){
-					return SELECTED;
-				}else{
-					return AUTH;
-				}
-			}else{
-				return UNAUTH;
-			}
-		}
-		const std::string getUser() const{
-			return user;
-		}
-		const std::string getMBox() const{
-			return mbox;
-		}
-		const std::string get_uuid() const{
-			return uuid;
-		}
-		// bool authenticate(const std::string& username, const std::string& password){
-		// 	AuthenticationProvider& provider = AuthenticationProvider::getInst<A>();
-		// 	if(provider.lookup(username) == false){
-		// 		return false;
-		// 	}
-		// 	if(provider.authenticate(username, password)){
-		// 		user = username;
-		// 		return true;
-		// 	}
-		// 	return false;
-		// }
-	};
-
-	// template <class D>
 	class IMAPProvider: public Pollster::Handler{
 	private:
+		const Config &config;
 		static std::map<int, IMAPClientState> states;
+		struct tls *tls = NULL;
+		struct tls_config *t_conf = NULL;
 		//ANY STATE
-		static void CAPABILITY(int rfd, std::string tag){
-			respond(rfd, "*", "CAPABILITY", "IMAP4rev1");
-			OK(rfd, tag, "CAPABILITY executed successfully");
-		}
-		static void NOOP(int rfd);
-		static void LOGOUT(int rfd);
+		void CAPABILITY(int rfd, std::string tag) const;
+		void NOOP(int rfd, std::string tag) const{ OK(rfd, tag, "NOOP executed successfully"); }
+		void LOGOUT(int rfd) const;
 		//UNAUTHENTICATED
-		static void STARTTLS(int rfd);
-		static void AUTHENTICATE(int rfd);
-		static void LOGIN(int rfd);
+		void STARTTLS(int rfd, std::string tag) const;
+		void AUTHENTICATE(int rfd) const;
+		void LOGIN(int rfd) const;
 		 //AUTENTICATED
-		static void SELECT(int rfd);
-		static void EXAMINE(int rfd);
-		static void CREATE(int rfd);
-		static void DELETE(int rfd);
-		static void RENAME(int rfd);
-		static void SUBSCRIBE(int rfd);
-		static void UNSUBSCRIBE(int rfd);
-		static void LIST(int rfd);
-		static void LSUB(int rfd);
-		static void STATUS(int rfd);
-		static void APPEND(int rfd);
+		void SELECT(int rfd) const;
+		void EXAMINE(int rfd) const;
+		void CREATE(int rfd) const;
+		void DELETE(int rfd) const;
+		void RENAME(int rfd) const;
+		void SUBSCRIBE(int rfd) const;
+		void UNSUBSCRIBE(int rfd) const;
+		void LIST(int rfd) const;
+		void LSUB(int rfd) const;
+		void STATUS(int rfd) const;
+		void APPEND(int rfd) const;
 		//SELECTED
-		static void CHECK(int rfd);
-		static void CLOSE(int rfd);
-		static void EXPUNGE(int rfd);
-		static void SEARCH(int rfd);
-		static void STORE(int rfd);
-		static void COPY(int rfd);
-		static void UID(int rfd);
+		void CHECK(int rfd) const;
+		void CLOSE(int rfd) const;
+		void EXPUNGE(int rfd) const;
+		void SEARCH(int rfd) const;
+		void STORE(int rfd) const;
+		void COPY(int rfd) const;
+		void UID(int rfd) const;
 
 
 		//RESPONSES
-		static inline void respond(int rfd, std::string tag, std::string code, std::string message){
-			sendMsg(rfd, tag + " "+ code +" " + message + "\n");
-		}
-
-		static void OK(int rfd, std::string tag, std::string message){
-			respond(rfd, tag, "OK", message + " " + states[rfd].get_uuid());
-		}
-		
-		static void NO(int rfd, std::string tag, std::string message){
-			respond(rfd, tag, "NO", message);
-		}
-
-		static void BAD(int rfd, std::string tag, std::string message){
-			respond(rfd, tag, "BAD", message);
-		}
-		static void PREAUTH(int rfd, std::string tag, std::string message){
-			respond(rfd, tag, "PREAUTH", message);
-		}
-
-		static void BYE(int rfd, std::string tag, std::string message){
-			respond(rfd, tag, "BYE", message);
-		}
-
-		// static std::map<int, IMAPClientState> map;
-
-		static void route(int fd, std::string tag, std::string command, std::string args){
-			std::transform(command.begin(), command.end(),command.begin(), ::toupper); //https://stackoverflow.com/questions/735204/convert-a-string-in-c-to-upper-case
-			if(command == "CAPABILITY"){
-				CAPABILITY(fd, tag);
+		inline void respond(int rfd, std::string tag, std::string code, std::string message) const{
+			if(states[rfd].tls == NULL){
+				sendMsg(rfd, tag + " "+ code +" " + message + "\n");
 			}else{
-				BAD(fd, tag, "Command \"" + command + "\" NOT FOUND");
+				sendMsg(states[rfd].tls, tag + " "+ code +" " + message + "\n");
 			}
 		}
 
-		static void parse(int fd, std::string message){
-			std::stringstream mstream(message);
-			std::string tag, command, args;
-			if(mstream >> tag >> command){
-				if(mstream >> args){
-					route(fd, tag, command, args);
-				}else{
-					route(fd, tag, command, "");
-				}
-			}else{
-				BAD(fd, "*", "Unable to parse command");
-			}
-		}
+		void OK(int rfd, std::string tag, std::string message) const{ respond(rfd, tag, "OK", message + " " + states[rfd].get_uuid()); }
+		void NO(int rfd, std::string tag, std::string message) const{ respond(rfd, tag, "NO", message); }
+		void BAD(int rfd, std::string tag, std::string message) const{ respond(rfd, tag, "BAD", message); }
+		void PREAUTH(int rfd, std::string tag, std::string message) const{ respond(rfd, tag, "PREAUTH", message); }
+		void BYE(int rfd, std::string tag, std::string message) const{ respond(rfd, tag, "BYE", message); }
+		void route(int fd, std::string tag, std::string command, std::string args) const;
+		void parse(int fd, std::string message) const;
 
 	public:
-		IMAPProvider(){
-			SSL_load_error_strings();	
-   			OpenSSL_add_ssl_algorithms();
+		IMAPProvider(Config &cfg) : config(cfg){
+			if(cfg.secure || cfg.starttls){
+				t_conf = tls_config_new();
+				tls = tls_server();
+				unsigned int protocols = 0;
+				if(tls_config_parse_protocols(&protocols, "all") < 0){
+					printf("tls_config_parse_protocols error\n");
+				}
+				tls_config_set_protocols(t_conf, protocols);
+				if(tls_config_set_ciphers(t_conf, cfg.ciphers) < 0) {\
+					printf("tls_config_set_ciphers error\n");
+				}
+				if(tls_config_set_key_file(t_conf, cfg.keypath) < 0) {
+					printf("tls_config_set_key_file error\n");
+				}
+				if(tls_config_set_cert_file(t_conf, cfg.certpath) < 0) {
+					printf("tls_config_set_cert_file error\n");
+				}
+				if(tls_configure(tls, t_conf) < 0) {
+					printf("tls_configure error: %s\n", tls_error(tls));
+				}
+			}
+
 		}
 		~IMAPProvider(){
-			EVP_cleanup();
+			if(t_conf != NULL){
+				tls_config_free(t_conf);
+			}
 		}
 		void operator()(int fd) const{
 			std::string data(8193, 0);
-			int rcvd = recv(fd, &data[0], 8192, MSG_DONTWAIT);
+
+
+			int rcvd;
+			if(states[fd].state() != UNENC){
+				rcvd = tls_read(states[fd].tls, &data[0], 8912);
+			}else{
+				rcvd= recv(fd, &data[0], 8192, MSG_DONTWAIT);
+			}
 			if( rcvd == -1){
 				disconnect(fd, "Unable to read from socket");
 			}else{
@@ -229,15 +114,98 @@ namespace IMAPProvider{
 		}
 		void disconnect(int fd, std::string reason) const{
 			BYE(fd, "*", reason);
+			if(states[fd].tls !=  NULL){
+				tls_close(states[fd].tls);
+				tls_free(states[fd].tls);
+			}
+			states.erase(fd);
 			close(fd);
 		}
-		void connect(int fd) const{
-			struct sockaddr_in addr;
-			socklen_t addrlen = sizeof(addr);
-			int getaddr = getpeername(fd, (struct sockaddr *) &addr, &addrlen);
-			std::string address(inet_ntoa(addr.sin_addr));
-			OK(fd, "*", "Welcome to IMAPlw. IMAP ready for requests from " + address);
-		}
+		void connect(int fd) const;
 	};
-	std::map<int, IMAPClientState> IMAPProvider::states;
 }
+
+void IMAPProvider::IMAPProvider::connect(int fd) const{
+	if(config.secure){
+		if(tls_accept_socket(tls, &states[fd].tls, fd) < 0) {
+			std::cout << "tls_accept_socket error" << std::endl;
+			disconnect(fd, "TLS Negotiation Failed");
+		}else{
+			if(tls_handshake(states[fd].tls) < 0){
+				std::cout << "tls_handshake error" << std::endl;
+				disconnect(fd, "TLS Negotiation Failed");
+			}else{
+				states[fd].starttls();
+			}
+		}
+	}
+	struct sockaddr_in addr;
+	socklen_t addrlen = sizeof(addr);
+	int getaddr = getpeername(fd, (struct sockaddr *) &addr, &addrlen);
+	std::string address(inet_ntoa(addr.sin_addr));
+	OK(fd, "*", "Welcome to IMAPlw. IMAP ready for requests from " + address);
+}
+
+
+
+std::map<int, IMAPProvider::IMAPClientState> IMAPProvider::IMAPProvider::states;
+
+void IMAPProvider::IMAPProvider::CAPABILITY(int rfd, std::string tag) const{
+	if(config.starttls && !config.secure && (states[rfd].state() == UNENC)){
+		respond(rfd, "*", "CAPABILITY", "IMAP4rev1 STARTTLS");
+	}else{
+		respond(rfd, "*", "CAPABILITY", "IMAP4rev1 ");
+	}
+	OK(rfd, tag, "CAPABILITY executed successfully");
+}
+void IMAPProvider::IMAPProvider::STARTTLS(int rfd, std::string tag) const{
+	if(config.starttls && !config.secure && (states[rfd].state() == UNENC)){
+		OK(rfd, tag, "Begin TLS Negotiation Now");
+		if(tls_accept_socket(tls, &states[rfd].tls, rfd) < 0) {
+			std::cout << "tls_accept_socket error" << std::endl;
+		}else{
+			if(tls_handshake(states[rfd].tls) < 0){
+				std::cout << "tls_handshake error" << std::endl;
+			}else{
+				states[rfd].starttls();
+			}
+		}
+	}else{
+		BAD(rfd, tag, "STARTTLS Disabled");
+	}
+}
+void IMAPProvider::IMAPProvider::route(int fd, std::string tag, std::string command, std::string args) const{
+	std::transform(command.begin(), command.end(),command.begin(), ::toupper); //https://stackoverflow.com/questions/735204/convert-a-string-in-c-to-upper-case
+	if(command == "CAPABILITY"){
+		CAPABILITY(fd, tag);
+	}else if(command == "STARTTLS"){
+		STARTTLS(fd, tag);
+	}else{
+		BAD(fd, tag, "Command \"" + command + "\" NOT FOUND");
+	}
+}
+void IMAPProvider::IMAPProvider::parse(int fd, std::string message) const{
+	std::stringstream mstream(message);
+	std::string tag, command, args;
+	if(mstream >> tag >> command){
+		if(mstream >> args){
+			route(fd, tag, command, args);
+		}else{
+			route(fd, tag, command, "");
+		}
+	}else{
+		BAD(fd, "*", "Unable to parse command");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
