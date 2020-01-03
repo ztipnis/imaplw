5a1a09c807c7a545ea72ff2cf48b6444c2ca4c07
Zachary Tipnis <zatipnis@icloud.com>
2020-01-02 22:32:28 -0500

    refactor to follow MVC

--- src/IMAP.cpp
+++ src/IMAP.cpp
@@ -1,18 +1,18 @@
-#import "IMAP/DataProvider.hpp"
+#import "IMAP/DataModel.hpp"
 #import <SocketPool.hpp>
 #import "IMAP/IMAPProvider.hpp"
 #import <iostream>
 
-class GAuthP : public AuthenticationProvider{
+class GAuthP : public IMAPProvider::AuthenticationModel{
 public:
 	bool lookup(std::string username){ return true; }
 	bool authenticate(std::string username, std::string password){ return true; }
 	std::string SASL(struct tls* fd, std::string mechanism){ return ""; }
-	GAuthP() : AuthenticationProvider("AUTH=PLAIN"){}
+	GAuthP() : AuthenticationModel("AUTH=PLAIN"){}
 };
-class DAuthP : public DataProvider{
+class DAuthP : public IMAPProvider::DataModel{
 public:
-	DAuthP() : DataProvider() {}
+	DAuthP() : DataModel() {}
 	selectResp select(const std::string& user, const std::string& mailbox){ selectResp r; return r;};
 	bool createMbox(const std::string& user, const std::string& mailbox){ return true; };
 	bool hasSubFolders(const std::string& user, const std::string& mailbox){return true;};
@@ -37,7 +37,7 @@ int main(int argc, char* argv[]){
 	std::cout << "Note: if # clients is not evently divisible by # threads, # clients will be truncated to be distibuted evenly amongst threads." << std::endl;
 	unsigned int port = 8080;
 	const char* address = "0.0.0.0";
-	IMAPProvider::Config c(true, false, "secure", "secure", "server.key", "server.crt");
+	IMAPProvider::ConfigModel c(true, false, "secure", "secure", "server.key", "server.crt");
 	IMAPProvider::IMAPProvider<GAuthP, DAuthP> p(c);
 	SocketPool sp(port, address, atoi(argv[1]), atoi(argv[2]), p, std::chrono::minutes(5));
 	while(1){
